299332b750eaee6bb6baece20ef921c66b2ccce0
daroOkpeku <68330153+daroOkpeku@users.noreply.github.com>
2020-09-26 17:33:06 +0100

    Add files via upload
    new flies

---
+++ build/precache-manifest.0dffadfa46611b84a0c6a88b6c62c3f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80701ea31312e7fa836d2c18a12eb5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95e952f66b8de1c588d3",
+    "url": "/static/css/main.e07c1adc.chunk.css"
+  },
+  {
+    "revision": "55cbfa3405f75de8dfab",
+    "url": "/static/js/2.8fee3ecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fee3ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e952f66b8de1c588d3",
+    "url": "/static/js/main.3cc91e22.chunk.js"
+  },
+  {
+    "revision": "a6cea561a05f2a3dd963",
+    "url": "/static/js/runtime-main.0939eb42.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/static/media/logo.12a6659f.svg"
+  }
+]);
